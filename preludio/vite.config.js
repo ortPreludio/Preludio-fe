d18bfb6ff941b9f3a0be44e5839bbb767480ac58
Ian Gregorini <47628155@comunidadort.edu.ar>
2025-11-25 15:33:12 -0300

    Update vite.config.js

--- preludio/vite.config.js
+++ preludio/vite.config.js
@@ -3,32 +3,27 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Preludio-fe/',
+  base: '/', // custom domain -> root
   plugins: [react()],
   server: {
     port: 5173,
-    define: {
-      'process.env': process.env
-    },
     proxy: {
       '/api': {
         target: process.env.VITE_PROXY_TARGET || 'http://localhost:3001',
-        changeOrigin: true
-      }
-    }
+        changeOrigin: true,
+      },
+    },
   },
   build: {
     sourcemap: false,
     minify: 'terser',
     terserOptions: {
-      compress: {
-        passes: 2
-      },
+      compress: { passes: 2 },
       mangle: true,
       format: {
         beautify: false,
-        comments: false
-      }
-    }
-  }
+        comments: false,
+      },
+    },
+  },
 })
